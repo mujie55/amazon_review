1c92556d67a5923ed88062f159ad1d26a16e1316
Lena He <deguomumu@gmail.com>
2019-09-06 03:14:21 +0200

    add sum_len

--- review.py
+++ review.py
@@ -7,8 +7,9 @@ with open('reviews.txt', 'r')as f:
 		if count % 10000 == 0:
 			print(len(data))
 
-print(len(data))
+print('档案读取完了,', '总共有', len(data), '条review')
 
-print(data[0])
-print('................')
-print(data[2])
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('review的平均长度为:', sum_len / len(data))
