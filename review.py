1ee9e9efec1dae7c366558145e90b36be185ef01
Lena He <deguomumu@gmail.com>
2019-09-06 17:14:28 +0200

    add <=100

--- review.py
+++ review.py
@@ -2,7 +2,7 @@ data = []
 count = 0
 with open('reviews.txt', 'r')as f:
 	for line in f:
-		data.append(line.strip())
+		data.append(line)
 		count += 1
 		if count % 10000 == 0:
 			print(len(data))
@@ -13,3 +13,11 @@ sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('review的平均长度为:', sum_len / len(data))
+
+new = []
+for d in data:
+	if len(d) <= 100:
+		new.append(d)
+print('留言长度小于100的一共有', len(new), '条')
+print(new[0])
+print(new[1])
